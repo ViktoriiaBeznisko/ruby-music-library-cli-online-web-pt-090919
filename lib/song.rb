89c422bff6e60a57299c16772cb17d8058dd03f7
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-10-20 23:47:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -57,7 +57,7 @@ class Song
   end
 
   def self.create_from_filename(filename)
-    new_from_filename(filename).select{|file| file.save}
+    new_from_filename(filename).tap{|file| file.save}
   end
 
 end
