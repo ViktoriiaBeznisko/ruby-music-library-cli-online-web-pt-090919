3da7c83162313d8367b02e5604685ef64d01ebee
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-10-20 22:52:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -40,9 +40,9 @@ class Song
     self.genre.songs << self unless genre.songs.include?(self)
   end
 
-  def self.find_by_name(name)
-    self.all.detect {|song| song.name == name}
-  end
+  # def self.find_by_name(name)
+  #   self.all.detect {|song| song.name == name}
+  # end
 
   def self.find_or_create_by_name(name)
       self.find_by_name(name) || self.create(name)
