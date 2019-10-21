1d1ade3da418524e7c4c515c6876019c15cb2b1d
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-10-21 00:26:32 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -5,26 +5,42 @@ class MusicLibraryController
     importer.import
   end
 
-  def call
-    loop do
+
+def call
+  input = ''
+
+  while input != 'exit'
+    puts "Welcome to your music library!"
+    puts "To list all of your songs, enter 'list songs'."
+    puts "To list all of the artists in your library, enter 'list artists'."
+    puts "To list all of the genres in your library, enter 'list genres'."
+    puts "To list all of the songs by a particular artist, enter 'list artist'."
+    puts "To list all of the songs of a particular genre, enter 'list genre'."
+    puts "To play a song, enter 'play song'."
+    puts "To quit, type 'exit'."
+    puts "What would you like to do?"
     input = gets.chomp
+
     case input
-    when "exit"
-      break
-    when "list songs"
-      list_songs
-    when "list artists"
-      list_artists
-    when "list genres"
-      list_genres
-    when "play song"
-      play_song(gets.chomp.to_i)
-    when "list artist"
-      list_songs_from_artist(gets.chomp)
-    when "list genre"
-      list_songs_from_genre(gets.chomp)
+    when 'list songs'
+      self.list_songs
+    when 'list artists'
+      self.list_artists
+    when 'list genres'
+      self.list_genres
+    when 'list artists'
+      self.list_artists
+    when 'list artist'
+      self.list_songs_by_artist
+    when 'list genre'
+      self.list_songs_by_genre
+    when 'play song'
+      self.play_song
+    else
+      "Type in a valid request please"
     end
   end
+
 end
 
 
