497adce021bdb15d055a379725d8ae364f300fd0
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-10-21 00:21:48 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -5,4 +5,31 @@ class MusicLibraryController
     importer.import
   end
 
+  def call
+      input = ""
+      until input == "exit"
+        puts "Please enter action"
+        input = gets.chomp
+        case input
+        when "list songs"
+          list_songs
+        when "list artists"
+          list_artists
+        when "list genres"
+          list_genres
+        when "play song"
+          play_song
+        when "list artist"
+          list_artist
+        when "list genre"
+          list_genre
+        when "exit"
+          puts "Goodbye"
+        else
+          puts "invalid action"
+        end
+      end
+    end
+
+
 end
